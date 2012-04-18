9271d2de02138583767f76c9a9b9094e8f996a09
Gurteshwar Singh <gurteshwarsingh@gmail.com>
2012-04-18 16:11:29 +0530

    v0.1dev

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup (name = 'FreeSWITCH-esl-python',
+	   version = '0.1vdev',
+	   packages = ['freeswitch-esl-python'],
+	   description = 'Auto-generated swig python module for FreeSWITCH mod_esl with a binary component.',
+	   )
