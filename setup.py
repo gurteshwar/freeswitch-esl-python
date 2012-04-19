6d1781916782b611c23f009b4bca22071c82dec6
Gurteshwar Singh <gurteshwarsingh@gmail.com>
2012-04-19 19:11:05 +0530

    Included required FreeSWITCH files

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
 
 setup (name = 'FreeSWITCH-esl-python',
-	   version = '0.1vdev',
-	   packages = ['freeswitchESL'],
-	   description = 'Auto-generated swig python module for FreeSWITCH mod_esl with a binary component.',
-	   )
+       version = '0.1vdev',
+       ext_modules=[Extension('_ESL',sources=['swig/esl_wrap.cpp',
+                                              'src/esl.c',
+                                              'src/esl_json.c',
+                                              'src/esl_event.c',
+                                              'src/esl_threadmutex.c',
+                                              'src/esl_config.c',
+                                              'src/esl_oop.cpp',
+                                              'src/esl_buffer.c'],
+       include_dirs=['include/'])],
+       packages = ['freeswitchESL'],
+       pymodules = ['ESL'],
+       description = 'Auto-generated swig python module for FreeSWITCH mod_esl with a binary component.',)
+	   
